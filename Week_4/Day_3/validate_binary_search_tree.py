ef8ecd058ee5cf7c843320742a16b0f68f15b680
rohanluthra123 <rohanluthra24318@gmail.com>
2026-07-18 23:35:39 +0530

    Add Validate Binary Search Tree solution

---
+++ Week_4/Day_3/validate_binary_search_tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def validate(node, min_val, max_val):
+            if not node:
+                return True
+
+            if not (min_val < node.val < max_val):
+                return False
+
+            return (
+                validate(node.left, min_val, node.val)
+                and
+                validate(node.right, node.val, max_val)
+            )
+
+        return validate(root, float('-inf'), float('inf'))
