29f1363666f80b536ca702aa2e1ad060ce97bf2f
rohanluthra123 <rohanluthra24318@gmail.com>
2026-07-18 23:33:35 +0530

    Add Search in a Binary Search Tree solution

---
+++ Week_4/Day_3/search_in_a_binary_search_tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if root.val == val:
+            return root
+
+        if val < root.val:
+            return self.searchBST(root.left, val)
+
+        return self.searchBST(root.right, val)
